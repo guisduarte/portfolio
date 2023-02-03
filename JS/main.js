9b0b4bd4999146eb0708f3835086ce05e8d5c100
Guilherme Santana <87387380+guisduarte@users.noreply.github.com>
2023-02-03 14:20:19 -0300

    atualização

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,20 @@
-let btn = document.querySelector(".btn-voltar");
-
-btn.addEventListener("click", function() {
+let btnVoltar = document.querySelector(".btn-voltar");
+btnVoltar.addEventListener("click", function() {
     window.scrollTo(0, 0,);
 });
+
+
+function rolar() {
+    let obj = event.target.dataset.nome;
+    document.getElementById(obj).scrollIntoView({behavior: "smooth"});    
+}
+
+function inicia() {    
+    document.querySelector("#btn-sobre-mim").addEventListener("click",rolar);
+    document.querySelector("#btn-portfolio").addEventListener("click",rolar);
+    document.querySelector("#btn-contato").addEventListener("click",rolar);
+
+}
+
+window.addEventListener("load", inicia);
+
