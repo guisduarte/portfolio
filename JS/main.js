9fe411ce807e429d4c407c84d45280d2dfcc34a9
Guilherme Santana <87387380+guisduarte@users.noreply.github.com>
2023-02-14 22:26:51 -0300

    atualização

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,55 @@
-let btnVoltar = document.querySelector(".btn-voltar");
+
+// Voltar ao topo
+
+const btnVoltar = document.querySelector(".btn-voltar");
 btnVoltar.addEventListener("click", function() {
-    window.scrollTo(0, 0,);
+    //window.scrollTo(0, 0,);
+    window.scroll({
+        top: 0,
+        behavior: "smooth",
+      });
 });
 
 
-function rolar() {
-    let obj = event.target.dataset.nome;
-    document.getElementById(obj).scrollIntoView({behavior: "smooth"});    
+//Rolar página 
+const menuItems = document.querySelectorAll('.menu a[href^="#"], .menu-lista__link a[href^="#"]' );
+
+menuItems.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick);
+})
+
+function getScrollTopByHref(element) {
+  const id = element.getAttribute('href');
+  return document.querySelector(id).offsetTop;
 }
 
-function inicia() {    
-    document.querySelector("#btn-sobre-mim").addEventListener("click",rolar);
-    document.querySelector("#btn-portfolio").addEventListener("click",rolar);
-    document.querySelector("#btn-contato").addEventListener("click",rolar);
-
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const to = getScrollTopByHref(event.target) - 80;
+  scrollToPosition(to);
 }
 
-window.addEventListener("load", inicia);
+function scrollToPosition(to) {
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  });
+  smoothScrollTo(0, to);
+}
 
+
+//Menu Hamburguer
+const botaoMenuHamburguer = document.getElementById("botao-menu-hamburguer");
+const menu = document.getElementById("container__menu-escondido");
+
+botaoMenuHamburguer.addEventListener("click", mostraMenu);
+
+function mostraMenu() {
+    if(menu.style.display === 'none') {
+        menu.style.display = 'block';
+        this.style.backgroundColor = 'aqua';
+    } else {
+        menu.style.display = 'none';
+        this.style.backgroundColor = '';
+    }
+}
