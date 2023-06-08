82aafe9b73eb4e161c6137254133130f96e597bc
Guilherme Santana <87387380+guisduarte@users.noreply.github.com>
2023-06-08 12:37:45 -0300

    atualização menu mobile.
    implementação da função de clique fora do menu mobile para fechar o menu.

--- JS/modules/menu-mobile.js
+++ JS/modules/menu-mobile.js
@@ -3,10 +3,18 @@ export default function menuMobile(){
    const menuBotao = document.querySelector('[data-menu="button"]');
    const menulist = document.querySelector('[data-menu="list"]');
 
+   document.onclick = function(event) {
+        const targetDataMenu = event.target.getAttribute('data-menu');
+
+        if (targetDataMenu !== "button" && targetDataMenu !== "list") {
+            menulist.classList.remove('active');
+            menuBotao.classList.remove('active');
+        }
+    }
+
    function toggleMenu() {
         menulist.classList.toggle('active');
         menuBotao.classList.toggle('active');       
     }
     menuBotao.addEventListener('click', toggleMenu);
 }
-
